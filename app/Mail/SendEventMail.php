98e78ae4b3887150619839676215b5cdac4c8331
webdeveloper510 <developerweb6@gmail.com>
2024-04-20 18:05:22 +0530

    agreement  mails

--- app/Mail/SendEventMail.php
+++ app/Mail/SendEventMail.php
@@ -8,6 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Storage;
 
 class SendEventMail extends Mailable
 {
