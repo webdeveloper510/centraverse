818543b4d15240dbbfb891b1cbe5341f23ef21ce
webdeveloper510 <developerweb6@gmail.com>
2024-05-08 10:26:44 +0000

    session

--- storage/framework/views/a8006660e00bde20f129474820af95b0.php
+++ storage/framework/views/a8006660e00bde20f129474820af95b0.php
@@ -472,7 +472,7 @@ $(document).ready(function() {
 
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('breadcrumb'); ?>
-<li class="breadcrumb-item"><a href="<?php echo e(route('dashboard')); ?>"><?php echo e(__('Home')); ?></a></li>
+<li class="breadcrumb-item"><a href="<?php echo e(route('dashboard')); ?>"><?php echo e(__('Dashboard')); ?></a></li>
 <li class="breadcrumb-item"><?php echo e(__('Settings')); ?></li>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('content'); ?>
